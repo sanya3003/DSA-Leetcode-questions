4bb30e6673e2d894ca34486da09b094ef668a5c1
sanya3003 <115348466+sanya3003@users.noreply.github.com>
2024-08-02 23:32:25 +0530

    Time: 73 ms (35.76%), Space: 83.1 MB (47.74%) - LeetHub

--- 2255-minimum-swaps-to-group-all-1s-together-ii/2255-minimum-swaps-to-group-all-1s-together-ii.cpp
+++ 2255-minimum-swaps-to-group-all-1s-together-ii/2255-minimum-swaps-to-group-all-1s-together-ii.cpp
@@ -2,21 +2,18 @@ class Solution {
 public:
     int minSwaps(vector<int>& nums) {
         int n = nums.size();
-        vector<int> temp(2*n);
-        for(int i = 0;i<2*n;i++){
-            temp[i]=nums[i%n];
-        }
+        
         int totalOnes = accumulate(begin(nums), end(nums), 0);
         int i = 0;
         int j = 0;
         int currOnes = 0;
         int maxCount = 0;
         while(j<2*n){
-            if(temp[j]==1){
+            if(nums[j%n]==1){
                 currOnes++;
             }
             if(j-i+1 > totalOnes){
-                currOnes -= temp[i];
+                currOnes -= nums[i%n];
                 i++;
             }
             maxCount = max(maxCount, currOnes);

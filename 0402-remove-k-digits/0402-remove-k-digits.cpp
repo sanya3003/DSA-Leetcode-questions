e24aee06865ea16018cc2378936f99a55c028521
sanya3003 <115348466+sanya3003@users.noreply.github.com>
2024-08-18 17:54:09 +0530

    Time: 11 ms (61.89%), Space: 10.3 MB (46.58%) - LeetHub

---
+++ 0402-remove-k-digits/0402-remove-k-digits.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string removeKdigits(string num, int k) {
+        stack<char> stack;
+        for(char digit:num){
+            while(!stack.empty() && k>0 && stack.top() > digit){
+                stack.pop();
+                k--;
+            }
+            stack.push(digit);
+        }
+        while(k>0 && !stack.empty()){
+            stack.pop();
+            k--;
+        }
+        string result;
+        while(!stack.empty()){
+            result += stack.top();
+            stack.pop();
+
+        }
+        reverse(result.begin(), result.end());
+
+        size_t pos = result.find_first_not_of('0');
+        result = (pos == string::npos) ? "0" : result.substr(pos);
+
+        return result;
+    }
+};
